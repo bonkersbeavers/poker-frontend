ab0afe0b340227a4340622cf2655f406ae3bb6d2
Dominika Matus <dominika.matus@uj.edu.pl>
2020-06-17 14:24:17 +0200

    Connect to GraphQL server via http and websockets

--- src/main.js
+++ src/main.js
@@ -6,21 +6,43 @@ import VueApollo from "vue-apollo";
 
 import { ApolloClient } from 'apollo-client'
 import { createHttpLink } from 'apollo-link-http'
+import { WebSocketLink } from 'apollo-link-ws';
 import { InMemoryCache } from 'apollo-cache-inmemory'
+import {split} from "apollo-link";
+import {getMainDefinition} from "apollo-utilities";
 
 Vue.use(VueApollo);
 
 Vue.config.productionTip = false;
 
 const httpLink = createHttpLink({
-  // You should use an absolute URL here
-  uri: 'http://localhost:8081/graphql/',
+  uri: 'http://localhost:9001/graphql/',
 });
+
+const wsLink = new WebSocketLink({
+  uri: `ws://localhost:9001/graphql/`,
+  options: {
+    reconnect: true
+  }
+});
+
+const link = split(
+  // split based on operation type
+  ({ query }) => {
+    const definition = getMainDefinition(query);
+    return (
+      definition.kind === 'OperationDefinition' &&
+      definition.operation === 'subscription'
+    );
+  },
+  wsLink,
+  httpLink,
+);
 
 const cache = new InMemoryCache();
 
 const client = new ApolloClient({
-    link: httpLink,
+    link,
     cache,
 });
 
